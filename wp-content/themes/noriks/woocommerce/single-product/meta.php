f015d2766c5a15b602978c581abea7fa14cb8a81
Jovo <jovo@noriks.com>
2026-05-25 07:15:43 +0000

    feat(size-chart): new majice size table image in product accordion
    - Added tabela-velikosti-majice.jpg to theme img folder
    - meta.php accordion (else/majice branch): replaced tablica_si.jpg with new theme-bundled image
    - mixed_bundle (orto) branch unchanged

--- wp-content/themes/noriks/woocommerce/single-product/meta.php
+++ wp-content/themes/noriks/woocommerce/single-product/meta.php
@@ -487,7 +487,7 @@ $is_mixed_bundle = has_term( array( 'black-friday', 'majice-in-boksarice-paketi'
           <?php else: ?>
       
       
-       <img src="https://noriks.com/si/wp-content/uploads/2026/04/tablica_si.jpg">
+       <img src="<?php echo get_template_directory_uri(); ?>/img/tabela-velikosti-majice.jpg">
         
             
         <?php endif; ?>
