b6b9ceac93f6cb7037c611eb9d12762228c0a1ef
Noriks (via Claude) <info@noriks.com>
2026-08-07 13:05:30 +0200

    Revert "KOMZIPS: lokalizirana tabela velikosti (prej se je prikazovala tabela za majice)"
    This reverts commit b59b9e1bf4c1fe1f4c15615d8c092bc8a0a4c8ee.

--- wp-content/themes/noriks/woocommerce/single-product/meta.php
+++ wp-content/themes/noriks/woocommerce/single-product/meta.php
@@ -676,11 +676,6 @@ $is_mixed_bundle = has_term( array( 'black-friday', 'majice-in-boksarice-paketi'
           
           
         
-        <?php // KOMZIPS: vlastna lokalizirana tabela velikosti (sicer pade v else = tabela za majice).
-        elseif ( function_exists('noriks_is_type') && noriks_is_type('kompresijske-nogavice', $current_product_id) ): ?>
-
-                  <img class="js-open-size-chart" style="cursor:pointer;" src="<?php echo get_template_directory_uri(); ?>/img/kompresijske-tabela-si.webp" alt="Tabela velikosti — kompresijske nogavice">
-
         <?php elseif(  $is_carape ): ?>
         
         
