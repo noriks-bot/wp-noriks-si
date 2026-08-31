f37c6b2d4d5944ae45058ee886115d6e73169d11
dejane <dejan.luznic@gmail.com>
2026-08-31 10:17:26 +0200

    SI: mreza kolekcij nazaj na 4 stolpce (toliko, kot je kartic)
    Commit 1f72fcf4 je ob dodajanju kartice Nogavice mrezo spremenil s 4 na 5
    stolpcev. Kartico sem odstranil, mreza pa je ostala pri 5 -> prazen peti
    stolpec in kartice niso zapolnile sirine.

--- wp-content/themes/noriks/template-homepage.php
+++ wp-content/themes/noriks/template-homepage.php
@@ -329,7 +329,7 @@ Preizkusi NORIKS po boljši vstopni ceni.
 /* ---- Grid ---- */
 .collections__grid {
   display: grid;
-  grid-template-columns: repeat(5, minmax(0, 1fr));
+  grid-template-columns: repeat(4, minmax(0, 1fr));
   gap: 10px;
 }
 
